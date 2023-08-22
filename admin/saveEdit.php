624c6230c9af5913f27f44892f06227b1fad6c8a
Felipe Ferreira <100241185+felipeov1@users.noreply.github.com>
2023-08-22 09:40:25 -0300

    fix: edit products

--- admin/saveEdit.php
+++ admin/saveEdit.php
@@ -3,12 +3,13 @@
     include_once('../db/connection.php');
 
     if(isset($_POST['update'])){
+        $id = $_POST['id'];
         $productName = $_POST['productName'];
         $productDescription = $_POST['productDescription'];
 
-        $sqlUpdate = "UPDATE products SET product_name='$productName' ,product_description='$productDescription' WHERE product_id='$productName'";
+        $sqlUpdate = "UPDATE products SET product_name='$productName' ,product_description='$productDescription' WHERE product_id='$id'";
 
         $result = $conn->query($sqlUpdate);
 
     }
-    hearder('Location: controleDeProdutos.php');
+header('Location: controleDeProdutos.php');
