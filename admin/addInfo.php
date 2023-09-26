9c84be8d0ecf996f6c354cc504f25f8e447cd5dd
Felipe Ferreira <100241185+felipeov1@users.noreply.github.com>
2023-09-25 22:34:55 -0300

    fix: function of add more of one information on table

--- admin/addInfo.php
+++ admin/addInfo.php
@@ -1,99 +1,113 @@
 <?php
-
 require_once '../db/connection.php';
 
-if (isset($_GET['id'])) {
-    $idReference = $_GET['id'];
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $idReference = $_POST['idReference'];
+    $productSmallInfo = $_POST['productSmallInfo'];
+    $productDescription = $_POST['productDescription'];
+    $tableNameInfos = $_POST['tableNameInfo'];
+    $tableInfos = $_POST['tableInfo'];
+
+
+    $stmt = $conn->prepare("INSERT INTO productsinformations (product_smallinfo, product_description, nameInfo, info, id_reference) VALUES (?, ?, ?, ?, ?)");
+
+    for ($i = 0; $i < count($tableNameInfos); $i++) {
+        $stmt->bind_param("ssssi", $productSmallInfo, $productDescription, $tableNameInfos[$i], $tableInfos[$i], $idReference);
+        $stmt->execute();
+    }
+
+    $stmt->close();
+    $conn->close();
+
+    echo "Informações adicionadas com sucesso.";
 }
-
-if (isset($_POST['submit'])) {
-    $idReference = $_POST["idReference"];
-    $productSmallInfo = $_POST["productSmallInfo"];
-    $productDescription = $_POST["productDescription"];
-    $tableNameInfo = $_POST["tableNameInfo"];
-    $tableInfo = $_POST["tableInfo"];
-
-    mysqli_query($conn, "INSERT INTO productsinformations(product_smallinfo, product_description, nameInfo, info, id_reference) VALUES('$productSmallInfo', '$productDescription', '$tableNameInfo', '$tableInfo', '$idReference')");
-}
-
 ?>
 
-<DOCTYPE html>
-    <html lang="en">
+<<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="addInfoStyle.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+    <title>Adicionar Informações Técnicas</title>
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="index.php"><img src="../img/logo.png" height="70px" alt="Imagem Logo"></a>
+            <a href="../index.php"><button class="btn btn-outline-danger" type="submit">Sair</button></a>
+        </div>
+    </nav>
 
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="addInfoStyle.css">
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet"
-            integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
-        <title>Document</title>
-    </head>
-
-    <body>
-        <nav class="navbar navbar-expand-lg bg-body-tertiary">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="index.php"><img src="../img/logo.png" height="70px" alt="Imagem Logo"></a>
-                <a href="../index.php"><button class="btn btn-outline-danger" type="submit">Sair</button></a>
-            </div>
-        </nav>
-
-        <div class="container">
-            <div class="form">
-                <form action="addInfo.php" method="POST" enctype="multipart/form-data">
-                    <div class="formHeader">
-                        <div class="returnBtn">
-                            <button><a href="controleDeProdutos.php">Voltar</a></button>
-                        </div>
-                        <div>
-                        </div>
+    <div class="container">
+        <div class="form">
+            <form action="addInfo.php" method="POST">
+                <div class="formHeader">
+                    <div class="returnBtn">
+                        <button><a href="controleDeProdutos.php">Voltar</a></button>
                     </div>
-                    <label>Descritivos:</label><br>
-                    <div class="inputGroup">
-                        <div class="inputBox">
-                            <label for="productSmallInfo">Característica principais:</label>
-                            <input type="text" name="productSmallInfo" id="productSmallInfo" required>
-                        </div>
-                    </div><br>
-                    <div class="inputGroup">
-                        <div class="inputBox">
-                            <label for="productDescription">Descrição:</label>
-                            <input type="text" name="productDescription" id="productDescription" required>
-                        </div>
-                    </div><br>
-                    <div>
+                </div>
+                <label>Descritivos:</label><br>
+                <div class="inputGroup">
+                    <div class="inputBox">
+                        <label for="productSmallInfo">Características principais:</label>
+                        <input type="text" name="productSmallInfo" id="productSmallInfo" required>
                     </div>
-                    <label>Tabela Técnica:</label><br>
-                    <div>
-
+                </div><br>
+                <div class="inputGroup">
+                    <div class="inputBox">
+                        <label for="productDescription">Descrição:</label>
+                        <input type="text" name="productDescription" id="productDescription" required>
+                    </div>
+                </div><br>
+                <label>Tabela Técnica:</label><br>
+                <div>
+                    <div id="dynamic-inputs">
                         <div class="inputGroup">
                             <div class="inputBox">
                                 <label for="tableNameInfo">Nome da especificação:</label>
-                                <input type="text" name="tableNameInfo" id="tableNameInfo" placeholder="ex: POTÊNCIA"
-                                    required>
+                                <input type="text" name="tableNameInfo[]" placeholder="ex: POTÊNCIA" required>
                             </div>
-                        </div>
-                        <div class="inputGroup">
                             <div class="inputBox">
                                 <label for="tableInfo">Característica da especificação:</label>
-                                <input type="text" name="tableInfo" id="tableInfo" placeholder="ex: 37,4 KW / 2.300 rpm"
-                                    required>
-
+                                <input type="text" name="tableInfo[]" placeholder="ex: 37,4 KW / 2.300 rpm" required>
                             </div>
                         </div>
-
-
-                        <input type="hidden" name="idReference" value="<?php echo $idReference; ?>">
-                        <div class="updateBtn">
-                            <input id="update" type="submit" value="Salvar" name="submit">
-                        </div>
-                </form>
-            </div>
+                    </div>
+                    <input class="addMore" type="button" onclick="addInput()" value="Adicionar Mais">
+                </div>
+                <input type="hidden" name="idReference" value="<?php echo $idReference; ?>">
+                <div class="updateBtn">
+                    <input id="update" type="submit" value="Salvar" name="submit">
+                </div>
+            </form>
         </div>
-    </body>
+    </div>
+
+    <script>
+        function addInput() {
+            let dynamicInputs = document.getElementById("dynamic-inputs");
+            let inputGroup = document.createElement("div");
+            inputGroup.classList.add("input-group");
+            inputGroup.innerHTML = `
+                <div class="inputBox">
+                    <label for="tableNameInfo">Nome da especificação:</label>
+                    <input type="text" name="tableNameInfo[]" placeholder="ex: POTÊNCIA" required>
+                </div>
+                <div class="inputBox">
+                    <label for="tableInfo">Característica da especificação:</label>
+                    <input type="text" name="tableInfo[]" placeholder="ex: 37,4 KW / 2.300 rpm" required>
+                </div>
+            `;
+            dynamicInputs.appendChild(inputGroup);
+        }
+    </script>
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm"
         crossorigin="anonymous"></script>
-
-    </html>
+</body>
+</html>
